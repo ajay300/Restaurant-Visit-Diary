42a20b129745213b684f2ffaec3fd08f24721a63
shrikant bhardwaj <ajayrajbhar300@gmail.com>
2022-06-30 10:24:52 +0530

    added

--- ThinkEasy/settings.py
+++ ThinkEasy/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 #     BASE_DIR / 'static'
 # # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
